192602162bd028ab90aece8063fbfc9115039810
xufulin1994 <fulin.xu@liveramp.com>
2023-02-17 15:39:54 +0800

    feat:test terraform

--- main.tf
+++ main.tf
@@ -11,5 +11,5 @@ provider "grafana" {
   alias = "cloud"
 
   url   = "https://qeteam.grafana.net"
-  auth  = "2122112"
+  auth  = "eyJrIjoiZGY3ZDViNTEzZTJjNzhiZWJjZWRhZDkzZGFmZTEwNmI3MDQ0ODZmMyIsIm4iOiJ0ZXN0dGVycmFmb3JtIiwiaWQiOjY4MjY2Nn0="
 }
