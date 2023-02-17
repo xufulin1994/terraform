4b0cace189423eaa00d488127c89286a799466aa
fulin <83196518+xufulin1994@users.noreply.github.com>
2023-02-17 14:44:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,5 +11,5 @@ provider "grafana" {
   alias = "cloud"
 
   url   = "https://qeteam.grafana.net"
-  auth  = "eyJrIjoiODE3OTZiMjIwMTYwNzFkMjJkMWVlYjAzNDAzNmQyZmU5MWI1Zjk3NCIsIm4iOiJ0ZXN0IiwiaWQiOjY4MjY2Nn0="
+  auth  = "eyJrIjoiZGY3ZDViNTEzZTJjNzhiZWJjZWRhZDkzZGFmZTEwNmI3MDQ0ODZmMyIsIm4iOiJ0ZXN0dGVycmFmb3JtIiwiaWQiOjY4MjY2Nn0="
 }
